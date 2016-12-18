5e86206e414d6ebd46c01472f936fc198fe4055c
baffour21 <kingsford.baffour@st.vvu.edu.gh>
2016-12-18 00:24:53 -0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -12,6 +12,7 @@ print_r($updateArray);
 $chatId = $updateArray["result"][0]["message"]["chat"]["id"];
 
 
-file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=control peee");  
+file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=KIGS");  
+file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=that's my nickname"); 
 
 ?>
